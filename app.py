ef4e81ddd78f91138e53893e1667cc3fecbc115e
Abishek2710 <abishek2710@gmail.com>
2022-03-16 02:38:02 +0530

    Adding gevent

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
 import pandas as pd
+
 app = Flask(__name__)
 
-
 def valid_input_check(username):
-
 	df = pd.read_csv(r"Data\sample30.csv")
 	list_of_valid_username  = df.reviews_username.unique().tolist()
 	return username in list_of_valid_username
