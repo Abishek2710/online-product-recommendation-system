551a8b6a7389fde47cd7be71dde01aebb99eb0ca
Abishek2710 <abishek2710@gmail.com>
2022-03-16 22:52:33 +0530

    Added dynamic URLs feature and alignment changes

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from model import ProductRecommendationUserLevel
 
 product_reco = ProductRecommendationUserLevel()
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST', "GET"])
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == "POST":
+        name = request.form['Username']
+        return redirect(url_for('submit', name=name))
     return render_template('homepage.html')
 
-@app.route("/submit", methods=['POST', "GET"])
-def submit():
-    name = request.form.get('Username')
+# Dynamic URLs based on user input
+@app.route('/predict/<name>', methods=['GET', 'POST'])
+def submit(name):
     if product_reco.valid_input_check(name):
         df = product_reco._get_recommendation_from_username_(name)
         return render_template('return.html',
-                               name=name,
-                               tables=[df.to_html(classes='data')],
-                               titles=df.columns.values
-                               )
+                                name=name,
+                                tables=[df.to_html(classes='data')],
+                                titles=df.columns.values
+                                )
     else:
         return render_template('returnfail.html', name=name)
 
